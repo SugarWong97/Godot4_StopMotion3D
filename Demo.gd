286157939051d4cb0cb45fa717ec77b4f7bc9d53
schips <schips@dingtalk.com>
2025-02-03 10:56:27 +0800

    test scene : using signal from MeshAnimationPlayer

--- Demo.gd
+++ Demo.gd
@@ -5,6 +5,8 @@ extends Node3D
 func _ready():
 	# initializes models, imports animations by name
 	mesh_animation_player.init()
+	mesh_animation_player.animation_frame_changed.connect(_when_mesh_animation_player_frame_changed)
+	mesh_animation_player.animation_finished.connect(_when_mesh_animation_player_animation_finished)
 
 	# Set delay for frames
 	mesh_animation_player.set_delayms(150)
@@ -20,3 +22,16 @@ func _ready():
 	#mesh_animation_player.pause() # pause
 	#mesh_animation_player.resume() # resume
 	#mesh_animation_player.stop() # stop
+
+func _when_mesh_animation_player_frame_changed(animName : StringName, frame : int):
+	print("Animation [" + animName + "] Played frame :" + str(frame))
+
+func _when_mesh_animation_player_animation_finished(animName : StringName) :
+	print("Animation [" + animName + "] finished")
+
+func _input(event):
+	if event is InputEventKey :
+		if Input.is_physical_key_pressed(KEY_Z) :
+			mesh_animation_player.play('run', true)
+		if Input.is_physical_key_pressed(KEY_X) :
+			mesh_animation_player.play('idle', false, true)
